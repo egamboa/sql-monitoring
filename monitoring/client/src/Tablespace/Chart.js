ccbeb9a265c4402cced5e9065af1bd9aafe932fd
Andres Gamboa <estebangam1@gmail.com>
2017-10-01 21:22:36 -0600

    ts pre work

--- monitoring/client/src/Tablespace/Chart.js
+++ monitoring/client/src/Tablespace/Chart.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
 class Chart extends Component {
+  barSize = 50;
+  chart = {};
+  chartHeight = 100;
   dataBusy = [];
   dataHighmark = [];
   dataMax = [];
-  chart = {};
+  labelFontSize = 16;
   realHighmark = [];
   realMax = [];
+  axisXinterval = 1;
+  lineThickness = 0;
 
   componentDidMount() {
     this.chart = new window.CanvasJS.Chart("chartContainer2", {
@@ -15,20 +20,20 @@ class Chart extends Component {
       },
       animationEnabled: true,
       axisX: {
-        interval: 1,
-        labelFontSize: 16,
-        lineThickness: 0
+        interval: this.axisXinterval,
+        labelFontSize: this.labelFontSize,
+        lineThickness: this.lineThickness
       },
       axisY2: {
         valueFormatString: "0 MB",
-        lineThickness: 0,
-        labelFontSize: 16
+        lineThickness: this.lineThickness,
+        labelFontSize: this.labelFontSize
       },
       toolTip: {
         shared: true
       },
       legend: {
-        fontSize: 16,
+        fontSize: this.labelFontSize,
         verticalAlign: "top",
         horizontalAlign: "center"
       },
@@ -62,17 +67,17 @@ class Chart extends Component {
     this.chart.render();
   }
 
-  componentDidUpdate() {
-    let busy, highmark, max, highmarkBar;
-
-    this.props.monitoring.forEach((ts, index) => {
+  chartShouldUpdate() {
+    let busy, highmark, max, highmarkBar, sortedTablespaces;
+    sortedTablespaces = this.props.monitoring.sort(ts => ts.tablespace);
+    sortedTablespaces.forEach((ts, index) => {
       if (this.dataBusy.length === this.props.monitoring.length) {
         this.dataBusy.shift(); this.dataHighmark.shift(); this.dataMax.shift();
       }
       busy = ts.max_size - ts.free;
       highmark = ts.max_size - (ts.max_size * (1 - (this.props.highmark / 100)));
       highmarkBar = highmark - busy;
-      if(highmarkBar > 0) {
+      if (highmarkBar > 0) {
         max = ts.max_size - (highmarkBar + busy);
       } else {
         highmarkBar = 0;
@@ -84,17 +89,24 @@ class Chart extends Component {
       this.realHighmark.push({ y: highmark, label: ts.tablespace });
       this.realMax.push({ y: ts.max_size, label: ts.tablespace });
     });
- 
+
     // this.chart.data[1].options.dataPoints = this.realHighmark;
     // this.chart.data[2].options.dataPoints = this.realMax;
-    console.log(this.chart.data[2].options);
-    
+
     this.chart.render();
+  }
+
+  componentWillUpdate() {
+    this.chartHeight = this.props.monitoring.length * this.barSize;
+  }
+
+  componentDidUpdate() {
+    this.chartShouldUpdate();
   }
 
   render() {
     return (
-      <div id="chartContainer2" style={{ height: 600 + "px", width: 100 + "%" }}></div>
+      <div id="chartContainer2" style={{ height: this.chartHeight + "px", width: 100 + "%" }}></div>
     );
   }
 }
