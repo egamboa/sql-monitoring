b4803325eeda730575df1ea085898e3168ff5aa2
Esteban Gamboa <estebangam1@gmail.com>
2018-11-08 15:07:20 +0000

    wip

--- monitoring/client/src/App.js
+++ monitoring/client/src/App.js
@@ -3,37 +3,12 @@ import { Grid, Row, Col } from 'react-bootstrap';
 import AppNav from './AppNav';
 import Main from './Main';
 
-import { SERVER_URL, CLIENT_VERSION, REACT_VERSION } from './config';
-import 'whatwg-fetch';
-
 class App extends Component {
 
-  constructor() {
-    super();
-
-    this.state = {
-      serverInfo: {},
-      clientInfo: {
-        version: CLIENT_VERSION,
-        react: REACT_VERSION
-      }
-    }
-  }
-
-  componentDidMount() {
-    fetch(SERVER_URL + 'application')
-      .then(r => r.json())
-      .then(json => this.setState({ serverInfo: json }))
-      .catch(error => console.error('Error connecting to server: ' + error));
-  }
-
   render() {
-    const serverInfo = this.state.serverInfo;
-    const clientInfo = this.state.clientInfo;
-
     return (
       <div>
-        <AppNav serverInfo={serverInfo} clientInfo={clientInfo} />
+        <AppNav/>
         <Grid>
           <section>
             <Row>
--- monitoring/client/src/AppNav.js
+++ monitoring/client/src/AppNav.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import { LinkContainer, IndexLinkContainer } from 'react-router-bootstrap'
-import { Navbar, NavDropdown, Nav, MenuItem, NavItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
-import grailsLogo from './images/grails-cupsonly-logo-white.svg';
-import 'whatwg-fetch';
-
-function AppNav ({serverInfo, clientInfo}) {
-
-  const {environment, appprofile, appversion, grailsversion, reloadingagentenabled, artefacts, plugins} = serverInfo; 
+function AppNav () {
 
   return (
     <Navbar inverse collapseOnSelect>
@@ -26,29 +21,6 @@ function AppNav ({serverInfo, clientInfo}) {
         <LinkContainer to="/logs">
           <NavItem>Logs</NavItem>
         </LinkContainer>
-        <Nav pullRight>
-          <NavDropdown eventKey="4" title="Application Status " id="app-status">
-            <MenuItem eventKey="4.1">Environment: {environment}</MenuItem>
-            <MenuItem eventKey="4.2">Grails profile: {appprofile}</MenuItem>
-            <MenuItem eventKey="4.2">Grails version: {grailsversion}</MenuItem>
-            <MenuItem eventKey="4.3">React version: {clientInfo.react.replace('^', '')}</MenuItem>
-            <MenuItem eventKey="4.3">Server version: {appversion}</MenuItem>
-            <MenuItem eventKey="4.3">Client version: {clientInfo.version}</MenuItem>
-            <MenuItem divider />
-            <MenuItem eventKey="4.4">Reloading active: {reloadingagentenabled ? 'true' : 'false'}</MenuItem>
-          </NavDropdown>
-          <NavDropdown eventKey="4" title="Artefacts " id="artefacts">
-            <MenuItem eventKey="4.1">Controllers: {artefacts ? artefacts.controllers : 0}</MenuItem>
-            <MenuItem eventKey="4.2">Domains: {artefacts ? artefacts.domains : 0}</MenuItem>
-            <MenuItem eventKey="4.3">Services: {artefacts ? artefacts.services : 0}</MenuItem>
-          </NavDropdown>
-          <NavDropdown eventKey="4" title="Installed Plugins " id="plugins">
-            {plugins ? plugins.map(plugin => {
-              return <MenuItem eventKey="4.1" key={plugin.name}>{plugin.name} - {plugin.version}</MenuItem>
-            }) : null
-            }
-          </NavDropdown>
-        </Nav>  
       </Nav>
     </Navbar>);
 }
